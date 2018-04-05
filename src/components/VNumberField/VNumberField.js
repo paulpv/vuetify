a753e5cabec648094fd02c6980e7d641313b88bc
Paul Peavyhouse <pv@swooby.com>
2018-04-04 21:13:36 -0700

    Removing code I don't think is necessary for numbers
    I think the only real questionible one is 'solo', and I can add that back in later.

--- src/components/VNumberField/VNumberField.js
+++ src/components/VNumberField/VNumberField.js
@@ -5,8 +5,6 @@ import '../../stylus/components/_text-fields.styl'
 // Mixins
 import Colorable from '../../mixins/colorable'
 import Input from '../../mixins/input'
-import Maskable from '../../mixins/maskable'
-import Soloable from '../../mixins/soloable'
 
 const dirtyTypes = ['color', 'file', 'time', 'date', 'datetime-local', 'week', 'month']
 
@@ -15,9 +13,7 @@ export default {
 
   mixins: [
     Colorable,
-    Input,
-    Maskable,
-    Soloable
+    Input
   ],
 
   inheritAttrs: false,
@@ -33,51 +29,27 @@ export default {
 
   props: {
     autofocus: Boolean,
-    autoGrow: Boolean,
     box: Boolean,
     clearable: Boolean,
     color: {
       type: String,
       default: 'primary'
     },
-    counter: [Boolean, Number, String],
     fullWidth: Boolean,
-    multiLine: Boolean,
-    noResize: Boolean,
     placeholder: String,
     prefix: String,
-    rowHeight: {
-      type: [Number, String],
-      default: 24,
-      validator: v => !isNaN(parseFloat(v))
-    },
-    rows: {
-      type: [Number, String],
-      default: 5,
-      validator: v => !isNaN(parseInt(v, 10))
-    },
-    singleLine: Boolean,
     suffix: String,
-    textarea: Boolean,
-    type: {
-      type: String,
-      default: 'text'
-    }
+    type: 'text'
   },
 
   computed: {
     classes () {
       const classes = {
-        ...this.genSoloClasses(),
         'input-group--text-field': true,
         'input-group--text-field-box': this.box,
-        'input-group--single-line': this.singleLine || this.isSolo,
-        'input-group--multi-line': this.multiLine,
         'input-group--full-width': this.fullWidth,
-        'input-group--no-resize': this.noResizeHandle,
         'input-group--prefix': this.prefix,
-        'input-group--suffix': this.suffix,
-        'input-group--textarea': this.textarea
+        'input-group--suffix': this.suffix
       }
 
       if (this.hasError) {
@@ -88,29 +60,13 @@ export default {
 
       return classes
     },
-    count () {
-      let inputLength
-      if (this.inputValue) inputLength = this.inputValue.toString().length
-      else inputLength = 0
-
-      return `${inputLength} / ${this.counterLength}`
-    },
-    counterLength () {
-      const parsedLength = parseInt(this.counter, 10)
-      return isNaN(parsedLength) ? 25 : parsedLength
-    },
     inputValue: {
       get () {
         return this.lazyValue
       },
       set (val) {
-        if (this.mask) {
-          this.lazyValue = this.unmaskText(this.maskText(this.unmaskText(val)))
-          this.setSelectionRange()
-        } else {
-          this.lazyValue = val
-          this.$emit('input', this.lazyValue)
-        }
+        this.lazyValue = val
+        this.$emit('input', this.lazyValue)
       }
     },
     isDirty () {
@@ -118,15 +74,6 @@ export default {
         this.lazyValue.toString().length > 0) ||
         this.badInput ||
         dirtyTypes.includes(this.type)
-    },
-    isTextarea () {
-      return this.multiLine || this.textarea
-    },
-    noResizeHandle () {
-      return this.isTextarea && (this.noResize || this.shouldAutoGrow)
-    },
-    shouldAutoGrow () {
-      return this.isTextarea && this.autoGrow
     }
   },
 
@@ -139,46 +86,22 @@ export default {
       }
     },
     value (val) {
-      if (this.mask && !this.internalChange) {
-        const masked = this.maskText(this.unmaskText(val))
-        this.lazyValue = this.unmaskText(masked)
-
-        // Emit when the externally set value was modified internally
-        String(val) !== this.lazyValue && this.$nextTick(() => {
-          this.$refs.input.value = masked
-          this.$emit('input', this.lazyValue)
-        })
-      } else this.lazyValue = val
+      this.lazyValue = val
 
       if (this.internalChange) this.internalChange = false
 
       !this.validateOnBlur && this.validate()
-      this.shouldAutoGrow && this.calculateInputHeight()
     }
   },
 
   mounted () {
-    this.shouldAutoGrow && this.calculateInputHeight()
     this.autofocus && this.focus()
   },
 
   methods: {
-    calculateInputHeight () {
-      this.inputHeight = null
-
-      this.$nextTick(() => {
-        const height = this.$refs.input
-          ? this.$refs.input.scrollHeight
-          : 0
-        const minHeight = parseInt(this.rows, 10) * parseFloat(this.rowHeight)
-        this.inputHeight = Math.max(minHeight, height)
-      })
-    },
     onInput (e) {
-      this.mask && this.resetSelections(e.target)
       this.inputValue = e.target.value
       this.badInput = e.target.validity && e.target.validity.badInput
-      this.shouldAutoGrow && this.calculateInputHeight()
     },
     blur (e) {
       this.isFocused = false
@@ -202,35 +125,17 @@ export default {
       this.$emit('focus', e)
     },
     keyDown (e) {
-      // Prevents closing of a
-      // dialog when pressing
-      // enter
-      if (this.isTextarea &&
-        this.isFocused &&
-        e.keyCode === 13
-      ) {
-        e.stopPropagation()
-      }
-
       this.internalChange = true
     },
-    genCounter () {
-      return this.$createElement('div', {
-        'class': {
-          'input-group__counter': true,
-          'input-group__counter--error': this.hasError
-        }
-      }, this.count)
-    },
     genInput () {
-      const tag = this.isTextarea ? 'textarea' : 'input'
+      const tag = 'input'
       const listeners = Object.assign({}, this.$listeners)
       delete listeners['change'] // Change should not be bound externally
 
       const data = {
         style: {},
         domProps: {
-          value: this.maskText(this.lazyValue)
+          value: this.lazyValue
         },
         attrs: {
           ...this.$attrs,
@@ -250,21 +155,9 @@ export default {
         ref: 'input'
       }
 
-      if (this.shouldAutoGrow) {
-        data.style.height = this.inputHeight && `${this.inputHeight}px`
-      }
-
       if (this.placeholder) data.attrs.placeholder = this.placeholder
 
-      if (!this.isTextarea) {
-        data.attrs.type = this.type
-      } else {
-        data.attrs.rows = this.rows
-      }
-
-      if (this.mask) {
-        data.attrs.maxlength = this.masked.length
-      }
+      data.attrs.type = this.type
 
       const children = [this.$createElement(tag, data)]
 
